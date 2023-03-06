949d15d9e6eca92e8f9e0937118c179bcdf8ebb1
Александр <hyperpositron@gmail.com>
2023-03-06 17:01:23 +0200

    create classes and getters setters

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book extends PrintedProduct {
+    private String name;
+    private String authorName;
+    private int pageQuantity;
+    private String content;
+
+    public void printContent() {
+        System.out.println(content);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getPageQuantity() {
+        return pageQuantity;
+    }
+
+    public void setPageQuantity(int pageQuantity) {
+        this.pageQuantity = pageQuantity;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/Magazine.java
@@ -0,0 +1,33 @@
+public class Magazine extends PrintedProduct {
+    private String name;
+    private int pageQuantity;
+    private String content;
+
+    public void printContent() {
+        System.out.println(content);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPageQuantity() {
+        return pageQuantity;
+    }
+
+    public void setPageQuantity(int pageQuantity) {
+        this.pageQuantity = pageQuantity;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
