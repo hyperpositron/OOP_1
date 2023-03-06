fcdd9025ef96650d7ea808a1ee84753d0828844c
Александр <hyperpositron@gmail.com>
2023-03-06 17:29:42 +0200

    не хочет b.printContent(); печатать

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Book b = new Book("War and Peace", 1000, "Lev Tolstoy", "War and Peace content");
+        b.printContent();
         }
 }
 
